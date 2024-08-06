0696636c7a585f61e30242b809a194aa9d0de157
RyanKusnadi2023 <ryankusnadi2002@gmail.com>
2024-08-06 10:53:40 +0700

    Initial Commit

---
+++ Mini 3 Coworking Trial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mini 3 Coworking Trial
+//
+//  Created by Ryan Kusnadi on 06/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mini 3 Coworking Trial/Mini_3_Coworking_TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_3_Coworking_TrialApp.swift
+//  Mini 3 Coworking Trial
+//
+//  Created by Ryan Kusnadi on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_3_Coworking_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
